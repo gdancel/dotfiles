dd4f0c84ab671f2688c723b902eda726a9582146
Gilo Dancel <dancel.gilo@gmail.com>
2022-12-09 16:01:21 +0800

    Remove unused files

--- macos.sh
+++
@@ -1,9 +0,0 @@
-# neovim
-curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-mkdir -p ~/.config/nvim
-cp .nvimrc ~/.config/nvim/init.vim
-
-# chunkwm  + khd
-cp .khdrc ~/.khdrc
-cp .chunkwmrc ~/.chunkwmrc
