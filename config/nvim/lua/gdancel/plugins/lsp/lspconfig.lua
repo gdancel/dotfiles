9c3b36ac67d65ea231437a655c476a36bd95e20b
Gilo Dancel <dancel.gilo@gmail.com>
2022-12-09 17:19:58 +0800

    Format files on write

--- config/nvim/lua/gdancel/plugins/lsp/lspconfig.lua
+++ config/nvim/lua/gdancel/plugins/lsp/lspconfig.lua
@@ -28,6 +28,8 @@ local on_attach = function(client, bufnr)
   keymap.set("n", "]d", "<cmd>Lspsaga diagnostic_jump_next<CR>", opts) -- jump to next diagnostic in buffer
   keymap.set("n", "K", "<cmd>Lspsaga hover_doc<CR>", opts) -- show documentation for what is under cursor
   keymap.set("n", "<leader>o", "<cmd>LSoutlineToggle<CR>", opts) -- see outline on right hand side
+
+  vim.cmd("au BufWritePre <buffer> lua vim.lsp.buf.format()")
 end
 
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
